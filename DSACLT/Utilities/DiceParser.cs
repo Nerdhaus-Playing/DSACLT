3b3dca4d8b2c040f5bf847dbb85c9e8e1d5e4106
Nerdhaus-Playing <Nerdhaus.playing@gmail.com>
2020-03-06 14:15:58 +0100

    Added Die and Diceparser

---
+++ DSACLT/Utilities/DiceParser.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSACLT.Utilities
+{
+    class DiceParser
+    {
+        static int ParseDie(string input)
+        {
+            int result = 0;
+            input.Replace(" ", "");
+            var components = input.Split('+');
+            foreach (var component in components)
+            {
+                if (component.Contains("W"))
+                {
+                    int diceCount = 1;
+                    var WIndex = component.IndexOf("W");
+                    if (WIndex != 0)
+                    {
+                        diceCount = Convert.ToInt32(component.Substring(0, WIndex));
+                    }
+                    int eyes = Convert.ToInt32(component.Substring(WIndex + 1));
+                    var die = new Die(eyes);
+                    for(int i = 0; i<diceCount;i++)
+                    {
+                        result += die.roll();
+                    }
+                }
+                else
+                {
+                    result += Convert.ToInt32(component);
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ DSACLT/Utilities/Die.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSACLT.Utilities
+{
+    class Die
+    {
+        public Die(int eyes)
+        {
+            Eyes = eyes;
+            if (random == null)
+            {
+                random = new Random();
+            }
+        }
+
+        public int roll()
+        {
+            return random.Next(1, Eyes);
+        }
+
+        public int Eyes { get; set; }
+        static private Random random;
+    }
+}
